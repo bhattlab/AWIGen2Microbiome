eef65f280b0094818e34c72a96bca57bcf4ce655
Jakob Wirbel <jakob.wirbel@gmail.com>
2023-02-06 09:05:47 -0800

    get only relevant numbers, in the correct format.

--- workflows/bin/count_deduplicate.py
+++ workflows/bin/count_deduplicate.py
@@ -14,7 +14,6 @@ parsed_json = parsed_json[0]
 reads_in = parsed_json.get('Paired_end').get('in')*2 + parsed_json.get('Single_end').get('in')
 reads_out = parsed_json.get('Paired_end').get('out')*2 + parsed_json.get('Single_end').get('out')
 
-print('\t'.join((sample_id, 'Deduplication input', str(reads_in))))
-print('\t'.join((sample_id, 'Deduplication output', str(reads_out))))
-print('\t'.join((sample_id, 'Duplicated reads', str(reads_in-reads_out))))
+print('\t'.join((sample_id, 'raw', str(reads_in))))
+print('\t'.join((sample_id, 'dedup', str(reads_out))))
 
