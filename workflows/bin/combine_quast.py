24d20320224fcd73fdbb7f5412211a8b93966dfd
Jakob Wirbel <jakob.wirbel@gmail.com>
2023-02-10 18:06:23 -0800

    try to combine quast results via python (the other things all failed :/

---
+++ workflows/bin/combine_quast.py
@@ -0,0 +1,21 @@
+#!/opt/software/miniconda3/bin/python3
+
+import sys
+import os
+import pandas as pd
+from functools import reduce
+
+out_dir = sys.argv[1]
+
+files_quast = os.listdir(out_dir)
+def read_quast(x):
+    sample = x.removesuffix('_report.tsv')
+    temp = pd.read_csv(out_dir + '/' + x, sep='\t')
+    temp.rename(columns={'Assembly': 'stats', temp.columns[1]: sample}, inplace=True)
+    return(temp)
+
+dfs = [read_mpa(f) for f in files_mpa]
+df_merged = reduce(lambda  left,right: pd.merge(left,right, on=['stats'],
+                                            how='outer'), dfs).fillna(0)
+df_merged.to_csv('quast_report.tsv', sep="\t", index=False)
+
